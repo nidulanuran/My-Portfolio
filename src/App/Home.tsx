4743573980e04e462c4b6864f32e3ef9bf21bce9
nidulanuran <nidulanuran22@gmail.com>
2026-07-23 19:22:45 +0530

    fix:add animation grid

--- src/App/Home.tsx
+++ src/App/Home.tsx
@@ -103,6 +103,7 @@ function Home(){
     return(
         <section id="home" className="relative min-h-screen flex items-center justify-center overflow-hidden">
 
+            <AnimatedGrid/>
 
             {/* Radial glow */}
             <div className="absolute inset-0 pointer-events-none">
