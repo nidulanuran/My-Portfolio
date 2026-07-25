5d334aefa3a735c2d6b057146d0bba7f091bed74
nidulanuran <nidulanuran22@gmail.com>
2026-07-25 12:31:07 +0530

    fix:loading problem

--- src/App/Navbar.tsx
+++ src/App/Navbar.tsx
@@ -35,7 +35,7 @@ function Navbar(){
             transition={{ duration: 0.6, ease: "easeOut" }}
             className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 
             ${scrolled
-            ? "bg-[#050a14]/90 backdrop-blur-md border-b border-[#00d4ff]/10"
+            ? "bg-[#050a14]/90 border-b border-[#00d4ff]/10"
             : "bg-transparent"
             }`}
         >
@@ -70,7 +70,7 @@ function Navbar(){
             initial={{ opacity: 0, height: 0 }}
             animate={{ opacity: 1, height: "auto" }}
             exit={{ opacity: 0, height: 0 }}
-            className="md:hidden overflow-hidden bg-[#050a14]/95 backdrop-blur-md border-b border-[#00d4ff]/10 px-6 pb-6"
+            className="md:hidden overflow-hidden bg-[#050a14]/95  border-b border-[#00d4ff]/10 px-6 pb-6"
           >
             <ul className="flex flex-col gap-4 pt-4">
               {NavLinks.map((link) => (
